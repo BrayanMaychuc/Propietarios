05e816678eb1b275b545593bdcaadb48f55a3657
BrayanMaychuc <brayanmay137@gmail.com>
2021-10-21 22:53:23 -0500

    Datos y Botones
    Hasta este punto ya concatenamos los datos del js con el html y se le agregaron botones

--- propietario.js
+++ propietario.js
@@ -4,10 +4,19 @@ new Vue({
 
 	//DEFINICION DEL ARRAY
 	data:{
-		propietarios:[{nombre:Jesus, apellidop:Chan, apellidom:tamay, genero:H},
-		{nombre:Maria, apellidop:Chable, apellidom:Burgos, genero:M},
-		{nombre:Ricardo, apellidop:Espadas, apellidom:Hu, genero:H},
-		{nombre:Antonio, apellidop:May, apellidom:Estrella, genero:H}
+		nombre:'',
+		apellidop:'',
+		apellidom:'',
+		genero:'',
+		editando:0,
+		indice:0,
+		buscar:'',
+		//TRABAJAREMOS UN ARRAY ESTÁTICO
+		propietarios:[{nombre:'Jesus', apellidop:'Chan', apellidom:'tamay', genero:'H'},
+		{nombre:'Maria', apellidop:'Chable', apellidom:'Burgos', genero:'M'},
+		{nombre:'Ricardo', apellidop:'Espadas', apellidom:'Hu', genero:'H'},
+		{nombre:'Antonio', apellidop:'May', apellidom:'Estrella', genero:'H'}
 		],
+
 	},
 })
