6e05281025c6f9b4ea35885b377eff6cb35e176c
BrayanMaychuc <brayanmay137@gmail.com>
2021-10-21 22:20:18 -0500

    Creación del Proyecto
    Se creo la carpeta y se agregaron los complementos, de igual forma se empezó a la creación de las secciones.

---
+++ propietario.js
@@ -0,0 +1,13 @@
+new Vue({
+	//ZONA DE ACTUACION DE VUE
+	el:"#miPagina",
+
+	//DEFINICION DEL ARRAY
+	data:{
+		propietarios:[{nombre:Jesus, apellidop:Chan, apellidom:tamay, genero:H},
+		{nombre:Maria, apellidop:Chable, apellidom:Burgos, genero:M},
+		{nombre:Ricardo, apellidop:Espadas, apellidom:Hu, genero:H},
+		{nombre:Antonio, apellidop:May, apellidom:Estrella, genero:H}
+		],
+	},
+})
